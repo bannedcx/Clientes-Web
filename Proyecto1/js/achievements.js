204951cb584595c79fcfe304e7289e2e554db392
SrNazaraK <59491741+SrNazaraK@users.noreply.github.com>
2026-06-09 17:35:39 -0400

    Create achievements.js

---
+++ Proyecto1/js/achievements.js
@@ -0,0 +1,6 @@
+const catalogologros = {
+    primerPaso: { titulo: "Primer Paso", icono: "🗺", desc: "Encuentra tu primer par." },
+    rachaCaliente: { titulo: "Buen Inicio", icono: "🔥", desc: "Encuentra 3 pares consecutivos." },
+    sinTitubeos: { titulo: "Preciso", icono: "🎯", desc: "Acierta en tu primer intento del juego." },
+    tanVelozComoUnaTortuga: { titulo: "Tan veloz como una tortuga", icono: "⚡️", desc: "Gana en menos de 45 segundos." }
+};
