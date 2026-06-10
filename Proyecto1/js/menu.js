3b45ae7da0d6cbdaea827e225e2e622cc11005bf
SrNazaraK <59491741+SrNazaraK@users.noreply.github.com>
2026-06-10 01:26:58 -0400

    Create menu.js

---
+++ Proyecto1/js/menu.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('setup-form');
+    const modeSelect = document.getElementById('game-mode');
+    const player2Wrapper = document.getElementById('player2-input-wrapper');
+    const player2Input = document.getElementById('player2-name');
