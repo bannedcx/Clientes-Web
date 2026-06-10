3dfff1a0b997e6faea065de769a33f081caf9bd9
SrNazaraK <59491741+SrNazaraK@users.noreply.github.com>
2026-06-10 15:51:08 -0400

    Configuración del menú menu.js

--- Proyecto1/js/menu.js
+++ Proyecto1/js/menu.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const modeSelect = document.getElementById('game-mode');
     const player2Wrapper = document.getElementById('player2-input-wrapper');
     const player2Input = document.getElementById('player2-name');
+
+    modeSelect.addEventListener('change', (e) => {
+        if (e.target.value === 'pvp') {
+            //cuando elijamos pvp, mostramos al segundo jugador
+            player2Wrapper.classList.remove('hidden');
+            player2Input.setAttribute('required', 'true'); 
+        } else {
+            //si no, ocultamos al jugador 2 y eliminamos su valor
+            player2Wrapper.classList.add('hidden');
+            player2Input.removeAttribute('required');
+        }
+    });
