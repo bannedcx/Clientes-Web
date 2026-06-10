f4ecac78ae5bd1315d9d0dc7a695ee7452bf59be
SrNazaraK <59491741+SrNazaraK@users.noreply.github.com>
2026-06-09 20:15:18 -0400

    Update achievements.js

--- Proyecto1/js/achievements.js
+++ Proyecto1/js/achievements.js
@@ -4,3 +4,28 @@ const catalogologros = {
     sinTitubeos: { titulo: "Preciso", icono: "🎯", desc: "Acierta en tu primer intento del juego." },
     tanVelozComoUnaTortuga: { titulo: "Tan veloz como una tortuga", icono: "⚡️", desc: "Gana en menos de 45 segundos." }
 };
+
+function desbloquearLogro(idLogro) {
+    const logro = catalogologros[idLogro];
+
+    if (!logro) {
+        console.error(el logro '${idLogro}' no existe.);
+        return;
+    }
+
+    if (gameState.unlockedAchievements.includes(idLogro)) return;
+
+    gameState.unlockedAchievements.push(idLogro);
+
+    mostrarNotificacionFlotante(logro);
+
+    const listaHUD = document.getElementById('achievements-list');
+    if (listaHUD) {
+        listaHUD.innerHTML += <li>${logro.icono} <strong>${logro.titulo}</strong></li>;
+    }
+    
+    const listaFinal = document.getElementById('end-achievements-list');
+    if(listaFinal) {
+        listaFinal.innerHTML += <li>${logro.icono} ${logro.titulo}: <small>${logro.desc}</small></li>;
+    }
+}
