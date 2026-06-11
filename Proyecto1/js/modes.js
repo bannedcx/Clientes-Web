a5c581762f015acc8f22d207f1e6c25cae7c3d56
SrNazaraK <59491741+SrNazaraK@users.noreply.github.com>
2026-06-10 21:11:49 -0400

    Create modes.js

---
+++ Proyecto1/js/modes.js
@@ -0,0 +1,24 @@
+function inicializarHUD() {
+    const playersStats = document.querySelector('.players-stats');
+    
+    if (gameState.mode === 'pvp') {
+        playersStats.innerHTML = `
+            <div id="p1-score-box" class="player-box active-turn">
+                <h4>1. ${gameState.players[0]}</h4>
+                <p>Pares: <span id="p1-score">0</span></p>
+            </div>
+            <div id="p2-score-box" class="player-box">
+                <h4>2. ${gameState.players[1]}</h4>
+                <p>Pares: <span id="p2-score">0</span></p>
+            </div>
+        `;
+    } else {
+        let textoModo = gameState.mode === 'solo' ? 'Cronometrado' : 'Práctica Libre';
+        playersStats.innerHTML = `
+            <div class="player-box">
+                <h4>${gameState.players[0]}</h4>
+                <p>Modo: ${textoModo}</p>
+            </div>
+        `;
+    }
+}
