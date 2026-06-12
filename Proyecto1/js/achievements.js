a5ebdf56820804c7c44aeac0d5aa947fde9caea3
bannedcx <ericenriquevargas20@gmail.com>
2026-06-11 23:59:10 -0400

    Update achievement time requirement for 'tanVelozComoUnaTortuga'

--- Proyecto1/js/achievements.js
+++ Proyecto1/js/achievements.js
@@ -2,7 +2,7 @@ const catalogologros = {
     primerPaso: { titulo: "Primer Paso", icono: "🗺", desc: "Encuentra tu primer par." },
     rachaCaliente: { titulo: "Buen Inicio", icono: "🔥", desc: "Encuentra 3 pares consecutivos." },
     sinTitubeos: { titulo: "Preciso", icono: "🎯", desc: "Acierta en tu primer intento del juego." },
-    tanVelozComoUnaTortuga: { titulo: "Tan veloz como una tortuga", icono: "⚡️", desc: "Gana en menos de 45 segundos." }
+    tanVelozComoUnaTortuga: { titulo: "Tan veloz como una tortuga", icono: "⚡️", desc: "Gana en menos de 30 segundos." }
 };
 
 function desbloquearLogro(idLogro) {
